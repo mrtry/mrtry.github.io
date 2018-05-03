f6fb73dc0433cecfa92d6e4acff2c0ece727fd0e
mrtry <yamamoto9302@gmail.com>
2018-05-03 12:52:24 +0900

    setup material-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
-import * as React from 'react';
-import './App.css';
+import * as React from 'react'
 
-import logo from './logo.svg';
+import Paper from 'material-ui/Paper'
+import Typography from 'material-ui/Typography'
+import 'typeface-roboto'
 
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
-    );
+    <div>
+      <Paper>
+        <Typography variant="headline" component="h3">
+          Shinnosuke Yamamoto
+          @mrtry
+        </Typography>
+      </Paper>
+    </div>
+    )
   }
 }
 
-export default App;
+export default App
